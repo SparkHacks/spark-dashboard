3b7a3148db01af5ad7a0e9a8c2b87b9245803b7c
Hamza G. <hamzagaziuddin04@gmail.com>
2025-02-03 20:09:33 -0600

    Qr code works

---
+++ src/components/AdminCode.tsx
@@ -0,0 +1,63 @@
+import { Html5QrcodeScanner } from 'html5-qrcode';
+import { useEffect } from 'react';
+
+const createConfig = (props: any) => {
+  let config: any = {};
+  if (props.fps) {
+      config.fps = props.fps;
+  }
+  if (props.qrbox) {
+      config.qrbox = props.qrbox;
+  }
+  if (props.aspectRatio) {
+      config.aspectRatio = props.aspectRatio;
+  }
+  if (props.disableFlip !== undefined) {
+      config.disableFlip = props.disableFlip;
+  }
+  return config;
+};
+
+const QrcodePlugin = (props: any) => {
+  useEffect(() => {
+    // when component mounts
+    const config = createConfig(props);
+    const verbose = props.verbose === true;
+    // Suceess callback is required.
+    if (!(props.qrCodeSuccessCallback)) {
+        throw "qrCodeSuccessCallback is required callback.";
+    }
+    const html5QrcodeScanner = new Html5QrcodeScanner("qrcodeRegionId", config, verbose);
+    html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
+
+    // cleanup function when component will unmount
+    return () => {
+        html5QrcodeScanner.clear().catch(error => {
+            console.error("Failed to clear html5QrcodeScanner. ", error);
+        });
+    };
+  }, []);
+
+  return (
+    <div>
+      <div id="qrcodeRegionId"></div>
+    </div>
+  )
+}
+
+export default function AdminCode() {
+  
+
+  return (
+    <div>
+      <QrcodePlugin
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          qrCodeSuccessCallback={(...a: any[]) => {
+            
+          }}
+      />
+    </div>
+  )
+}
