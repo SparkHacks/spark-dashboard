f92630ae00961b7279deabd6cac013ea180af895
Hamza G. <hamzagaziuddin04@gmail.com>
2025-02-04 07:19:29 -0600

    yes

--- src/components/AdminCode.tsx
+++ src/components/AdminCode.tsx
@@ -68,7 +68,6 @@ export default function AdminCode() {
     try {
       if(code === emailRef.current) return
       const docSnap = await getDoc(doc(db, "Forms", code))
-      console.log(docSnap)
       
       if (!docSnap.exists()) {
         alert("doc not exist")
@@ -80,8 +79,11 @@ export default function AdminCode() {
       setUserInfo(userData)
       toast.success(`Scanned user: ${userData.firstName} ${userData.lastName}`)
     } catch(e: any) {
+      // Prevent spamming
+      emailRef.current = code
       console.log(e)
       toast.error("Error scanning user", e)
+      setUserInfo(null)
     }
   }
 
@@ -93,9 +95,77 @@ export default function AdminCode() {
         disableFlip={false}
         qrCodeSuccessCallback={getUser}
       />
-      {userInfo && <div>
-        <p>Name: {userInfo.firstName} {userInfo.lastName}</p>
-        <p>Status: {userInfo.appStatus}</p>
+      {userInfo && <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', gap: '10px'}}>
+        <span>Name: {userInfo.firstName} {userInfo.lastName}</span>
+        <span>Status: {userInfo.appStatus}</span>
+        <span>Email: {userInfo.email}</span>
+        <span>Current Food Data</span>
+        <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
+          <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
+            <span>Day 1:</span>
+            <div className=''>
+              <label htmlFor="d1Snack">Ate Snack?</label>
+              <input id="d1Snack" type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d1Dinner">Ate Dinner?</label>
+              <input id="d1Dinner" type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d1Cookies">Ate Cookies?</label>
+              <input id="d1Cookies" type="checkbox"/>
+            </div>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
+            <span>Day 2:</span>
+            <div>
+              <label htmlFor="d2Breakfast">Ate Breakfast?</label>
+              <input id="d2Breakfast" type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d2Lunch">Ate Lunch?</label>
+              <input id="d2Lunch" type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d2Dinner">Ate Dinner?</label>
+              <input id="d2Dinner" type="checkbox"/>
+            </div>
+          </div>
+        </div>
+        <input type="button" value="Submit"/>
+        <span>Old Food Data</span>
+        <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
+          <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
+            <span>Day 1:</span>
+            <div>
+              <label htmlFor="d1SnackOld">Ate Snack?</label>
+              <input id="d1SnackOld" disabled type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d1DinnerOld">Ate Dinner?</label>
+              <input id="d1DinnerOld" disabled type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d1CookiesOld">Ate Cookies?</label>
+              <input id="d1CookiesOld" disabled type="checkbox"/>
+            </div>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
+            <span>Day 2:</span>
+            <div>
+              <label htmlFor="d2BreakfastOld">Ate Breakfast?</label>
+              <input id="d2BreakfastOld" disabled type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d2LunchOld">Ate Lunch?</label>
+              <input id="d2LunchOld" disabled type="checkbox"/>
+            </div>
+            <div>
+              <label htmlFor="d2DinnerOld">Ate Dinner?</label>
+              <input id="d2DinnerOld" disabled type="checkbox"/>
+            </div>
+          </div>
+        </div>
       </div>}
       <ToastContainer />
     </div>
