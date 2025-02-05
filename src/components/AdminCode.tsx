e5a6e718a07e63e3b97e7ac3cee9ab7fb95ae9e5
Hamza G. <hamzagaziuddin04@gmail.com>
2025-02-04 22:15:38 -0600

    Hamza changes

--- src/components/AdminCode.tsx
+++ src/components/AdminCode.tsx
@@ -1,6 +1,6 @@
 import { Html5QrcodeScanner } from 'html5-qrcode';
 import { useEffect, useRef, useState } from 'react';
-import { collection, doc, getCountFromServer, getDoc, getDocs, limit, orderBy, query, startAfter, where, type DocumentData } from "firebase/firestore"
+import { collection, doc, getCountFromServer, getDoc, getDocs, limit, orderBy, query, setDoc, startAfter, where, type DocumentData } from "firebase/firestore"
 import { db } from "../firebase/client"
 import type { FunctionResponse } from 'firebase/vertexai';
 import type { FormViewData } from '../env';
@@ -62,19 +62,20 @@ function debounce(cb: Function, delay: number) {
 }
 
 export default function AdminCode() {
-  const [userInfo, setUserInfo] = useState<FormViewData | null>(null)
+  const [userInfo, setUserInfo] = useState<any | null>(null)
   const emailRef = useRef<string | null>(null)
   const getUser = async (code: string) => {
     try {
       if(code === emailRef.current) return
-      const docSnap = await getDoc(doc(db, "Forms", code))
+      const docRef = doc(db, "Forms", code)
+      const docSnap = await getDoc(docRef)
       
       if (!docSnap.exists()) {
         alert("doc not exist")
         return
       } 
 
-      const userData = convertDocToFormViewData(docSnap)
+      const userData = docSnap.data()
       emailRef.current = userData.email
       setUserInfo(userData)
       toast.success(`Scanned user: ${userData.firstName} ${userData.lastName}`)
@@ -85,6 +86,24 @@ export default function AdminCode() {
       toast.error("Error scanning user", e)
       setUserInfo(null)
     }
+  }
+
+  async function submitFoodData() {
+    if (!userInfo) {
+      toast.error("No user info")
+      return
+    }
+    const data = [...document.querySelectorAll("#form input")].reduce((acc: any, curr: any) => (acc[curr.name] = curr.checked, acc), {})
+    data.email = userInfo.email
+    const response = await fetch("/api/auth/update-food", {
+      method: "POST",
+      body: JSON.stringify(data)
+    })
+    if (!response.ok) {
+      toast.error("Failed to update food data")
+      return
+    }
+    toast.success("Updated Food data!")
   }
 
   return (
@@ -100,79 +119,72 @@ export default function AdminCode() {
         <span>Status: {userInfo.appStatus}</span>
         <span>Email: {userInfo.email}</span>
         <span>Current Food Data</span>
-        <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
+        <div id='form' style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
           <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
             <span>Day 1:</span>
-            <div className=''>
-              <input id="d1Snack" type="checkbox"/>
+            <div>
+              <input name='d1Snack' id="d1Snack"  type="checkbox" defaultChecked={userInfo.d1Snack}/>
               <label htmlFor="d1Snack">Ate Snack?</label>
             </div>
             <div>
-              <input id="d1Dinner" type="checkbox"/>
+              <input name='d1Dinner' id="d1Dinner" type="checkbox" defaultChecked={userInfo.d1Dinner}/>
               <label htmlFor="d1Dinner">Ate Dinner?</label>
             </div>
             <div>
-              <input id="d1Cookies" type="checkbox"/>
+              <input name='d1Cookies' id="d1Cookies" type="checkbox" defaultChecked={userInfo.d1Cookies}/>
               <label htmlFor="d1Cookies">Ate Cookies?</label>
             </div>
           </div>
           <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
             <span>Day 2:</span>
             <div>
-              <input id="d2Breakfast" type="checkbox"/>
+              <input name='d2Breakfast' id="d2Breakfast" type="checkbox" defaultChecked={userInfo.d2Breakfast}/>
               <label htmlFor="d2Breakfast">Ate Breakfast?</label>
             </div>
             <div>
-              <input id="d2Lunch" type="checkbox"/>
+              <input name='d2Lunch' id="d2Lunch" type="checkbox" defaultChecked={userInfo.d2Lunch}/>
               <label htmlFor="d2Lunch">Ate Lunch?</label>
             </div>
             <div>
-              <input id="d2Dinner" type="checkbox"/>
+              <input name='d2Dinner' id="d2Dinner" type="checkbox" defaultChecked={userInfo.d1Dinner}/>
               <label htmlFor="d2Dinner">Ate Dinner?</label>
             </div>
           </div>
         </div>
-        <input type="button" value="Submit"/>
         <span>Old Food Data</span>
         <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
           <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
             <span>Day 1:</span>
             <div>
-              <input id="d1SnackOld" disabled type="checkbox"/>
+              <input id="d1SnackOld" disabled type="checkbox" defaultChecked={userInfo.d1Snack}/>
               <label htmlFor="d1SnackOld">Ate Snack?</label>
-              
             </div>
-
             <div>
-              <input id="d1DinnerOld" disabled type="checkbox"/>
+              <input id="d1DinnerOld" disabled type="checkbox" defaultChecked={userInfo.d1Dinner}/>
               <label htmlFor="d1DinnerOld">Ate Dinner?</label>
             </div>
-
             <div>
-              <input id="d1CookiesOld" disabled type="checkbox"/>
+              <input id="d1CookiesOld" disabled type="checkbox" defaultChecked={userInfo.d1Cookies}/>
               <label htmlFor="d1CookiesOld">Ate Cookies?</label>
             </div>
-
           </div>
-          <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
+          <div style={{display: 'flex', flexDirection: 'column', gap: '5px', padding: '0 0 10px 0'}}>
             <span>Day 2:</span>
             <div>
-              <input id="d2BreakfastOld" disabled type="checkbox"/>
+              <input id="d2BreakfastOld" disabled type="checkbox" defaultChecked={userInfo.d2Breakfast}/>
               <label htmlFor="d2BreakfastOld">Ate Breakfast?</label>
-              
             </div>
             <div>
-              <input id="d2LunchOld" disabled type="checkbox"/>
+              <input id="d2LunchOld" disabled type="checkbox" defaultChecked={userInfo.d2Lunch}/>
               <label htmlFor="d2LunchOld">Ate Lunch?</label>
-              
             </div>
             <div>
-              <input id="d2DinnerOld" disabled type="checkbox"/>
+              <input id="d2DinnerOld" disabled type="checkbox" defaultChecked={userInfo.d2Dinner}/>
               <label htmlFor="d2DinnerOld">Ate Dinner?</label>
-              
             </div>
           </div>
         </div>
+        <button onClick={submitFoodData} style={{marginTop: "20px", width: "250px", height: "40px", marginBottom: "20px"}}>Submit!</button>
       </div>}
       <ToastContainer />
     </div>
