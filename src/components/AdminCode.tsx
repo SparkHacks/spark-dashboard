5b6a6ad35e1da29da6d2c8190a58a25d804a6919
Hamza G. <hamzagaziuddin04@gmail.com>
2025-02-06 19:29:34 -0600

    Update users properly

--- src/components/AdminCode.tsx
+++ src/components/AdminCode.tsx
@@ -61,6 +61,7 @@ function debounce(cb: Function, delay: number) {
   };
 }
 
+const checkboxInputs = ["d1Snack", "d1Dinner", "d1Cookies", "d2Breakfast", "d2Lunch", "d2Dinner"]
 export default function AdminCode() {
   const [userInfo, setUserInfo] = useState<any | null>(null)
   const emailRef = useRef<string | null>(null)
@@ -81,6 +82,8 @@ export default function AdminCode() {
       emailRef.current = userData.email
       setUserInfo(userData)
       toast.success(`Scanned user: ${userData.firstName} ${userData.lastName}`)
+
+      checkboxInputs.forEach((input) => [...document.querySelectorAll(`[data-id="${input}"]`)].forEach((el: any) => { el.checked = userData[input] }))        
     } catch(e: any) {
       // Prevent spamming
       emailRef.current = code
@@ -132,39 +135,39 @@ export default function AdminCode() {
           <button onClick={searchUser}>Submit</button>
         </div>
       </div>
-      {userInfo && <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', gap: '10px'}}>
-        <span>Name: {userInfo.firstName} {userInfo.lastName}</span>
-        <span>Status: {userInfo.appStatus}</span>
-        <span>Email: {userInfo.email}</span>
+      {<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', gap: '10px'}}>
+        <span>Name: {userInfo?.firstName} {userInfo?.lastName}</span>
+        <span>Status: {userInfo?.appStatus}</span>
+        <span>Email: {userInfo?.email}</span>
         <span>Current Food Data</span>
         <div id='form' style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
           <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
             <span>Day 1:</span>
             <div>
-              <input name='d1Snack' id="d1Snack"  type="checkbox" defaultChecked={userInfo.d1Snack}/>
+              <input name='d1Snack' id="d1Snack"  type="checkbox" data-id="d1Snack"/>
               <label htmlFor="d1Snack">Ate Snack?</label>
             </div>
             <div>
-              <input name='d1Dinner' id="d1Dinner" type="checkbox" defaultChecked={userInfo.d1Dinner}/>
+              <input name='d1Dinner' id="d1Dinner" type="checkbox" data-id="d1Dinner"/>
               <label htmlFor="d1Dinner">Ate Dinner?</label>
             </div>
             <div>
-              <input name='d1Cookies' id="d1Cookies" type="checkbox" defaultChecked={userInfo.d1Cookies}/>
+              <input name='d1Cookies' id="d1Cookies" type="checkbox" data-id="d1Cookies"/>
               <label htmlFor="d1Cookies">Ate Cookies?</label>
             </div>
           </div>
           <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
             <span>Day 2:</span>
             <div>
-              <input name='d2Breakfast' id="d2Breakfast" type="checkbox" defaultChecked={userInfo.d2Breakfast}/>
+              <input name='d2Breakfast' id="d2Breakfast" type="checkbox" data-id="d2Breakfast"/>
               <label htmlFor="d2Breakfast">Ate Breakfast?</label>
             </div>
             <div>
-              <input name='d2Lunch' id="d2Lunch" type="checkbox" defaultChecked={userInfo.d2Lunch}/>
+              <input name='d2Lunch' id="d2Lunch" type="checkbox" data-id="d2Lunch"/>
               <label htmlFor="d2Lunch">Ate Lunch?</label>
             </div>
             <div>
-              <input name='d2Dinner' id="d2Dinner" type="checkbox" defaultChecked={userInfo.d2Dinner}/>
+              <input name='d2Dinner' id="d2Dinner" type="checkbox" data-id="d2Dinner"/>
               <label htmlFor="d2Dinner">Ate Dinner?</label>
             </div>
           </div>
@@ -174,35 +177,35 @@ export default function AdminCode() {
           <div style={{display: 'flex', flexDirection: 'column', gap: '5px'}}>
             <span>Day 1:</span>
             <div>
-              <input id="d1SnackOld" disabled type="checkbox" defaultChecked={userInfo.d1Snack}/>
+              <input id="d1SnackOld" disabled type="checkbox" data-id="d1Snack"/>
               <label htmlFor="d1SnackOld">Ate Snack?</label>
             </div>
             <div>
-              <input id="d1DinnerOld" disabled type="checkbox" defaultChecked={userInfo.d1Dinner}/>
+              <input id="d1DinnerOld" disabled type="checkbox" data-id="d1Dinner"/>
               <label htmlFor="d1DinnerOld">Ate Dinner?</label>
             </div>
             <div>
-              <input id="d1CookiesOld" disabled type="checkbox" defaultChecked={userInfo.d1Cookies}/>
+              <input id="d1CookiesOld" disabled type="checkbox" data-id="d1Cookies"/>
               <label htmlFor="d1CookiesOld">Ate Cookies?</label>
             </div>
           </div>
           <div style={{display: 'flex', flexDirection: 'column', gap: '5px', padding: '0 0 10px 0'}}>
             <span>Day 2:</span>
             <div>
-              <input id="d2BreakfastOld" disabled type="checkbox" defaultChecked={userInfo.d2Breakfast}/>
+              <input id="d2BreakfastOld" disabled type="checkbox" data-id="d2Breakfast"/>
               <label htmlFor="d2BreakfastOld">Ate Breakfast?</label>
             </div>
             <div>
-              <input id="d2LunchOld" disabled type="checkbox" defaultChecked={userInfo.d2Lunch}/>
+              <input id="d2LunchOld" disabled type="checkbox" data-id="d2Lunch"/>
               <label htmlFor="d2LunchOld">Ate Lunch?</label>
             </div>
             <div>
-              <input id="d2DinnerOld" disabled type="checkbox" defaultChecked={userInfo.d2Dinner}/>
+              <input id="d2DinnerOld" disabled type="checkbox" data-id="d2Dinner"/>
               <label htmlFor="d2DinnerOld">Ate Dinner?</label>
             </div>
           </div>
         </div>
-        <button onClick={submitFoodData} style={{marginTop: "20px", width: "250px", height: "40px", marginBottom: "20px"}}>Submit!</button>
+        <button onClick={submitFoodData} style={{marginTop: "20px", width: "250px", height: "40px", marginBottom: "20px"}} disabled={userInfo === null}>Submit!</button>
       </div>}
       <ToastContainer />
     </div>
