95ed02093e84b088fa983800bac650f1e0627b00
Daniel Barajas <danbarajas7@gmail.com>
2025-09-30 21:57:01 -0500

    Optimized original styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,19 +2,7 @@ import "./Sidebar.css";
 
 export default function Sidebar() {
   return (
-    <>
-      <div
-        style={{
-          width: "220px",
-          minHeight: "70vh",
-          backgroundColor: "#F7F7F7",
-          color: "black",
-          display: "flex",
-          flexDirection: "column",
-          padding: "20px",
-          borderRadius: "10px",
-        }}
-      >
+      <div id="sidebar">
         <h2 className="sidebar-menu">Menu</h2>
         <ul className="sidebar-menu-list">
           <li>
@@ -24,7 +12,7 @@ export default function Sidebar() {
             <a href="/admin" className="">Applicants</a>
           </li>
           <li>
-            <a href="/admin/qr" className="">QR Code</a>
+            <a href="/admin/qr" className="">QR Scanner</a>
           </li>
           <li>
             <a href="/admin/addadmin" className="">Add Admin</a>
@@ -40,7 +28,6 @@ export default function Sidebar() {
           </li>
         </ul>
       </div>
-    </>
   );
 }
 
